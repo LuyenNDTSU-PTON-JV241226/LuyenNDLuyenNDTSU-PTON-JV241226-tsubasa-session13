d7ac38aabb2c24bedcd0f881f6766c7a65e5bc53
LuyenNDTSU-PTON-JV241226 <dinhluyen2402@gmail.com>
2025-02-17 22:32:00 +0900

    upload btvn ss12-2

---
+++ ss13-2.js
@@ -0,0 +1,7 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let x = +prompt("Hãy nhập vào số bạn muốn");
+if(numbers.includes(x)){
+    alert("Bingooo!!!");
+}else {
+    alert("Chúc bạn may mắn lần sau")
+}
